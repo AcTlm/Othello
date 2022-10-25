0d2dcc8e628cb760cce3fa6f786d1182c6ada958
amine2amayes <amine2amayes@gmail.com>
2022-10-25 10:57:51 +0200

    Ajout fonctions plateau

--- board_class.py
+++ board_class.py
@@ -3,17 +3,19 @@ import numpy as np
 #1 = case noire
 #2 = case blanche
 class Plateau:
+    liste_id_joueurs=[1,2]
+    liste_directions=[[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
     dict_equivalence = {
             0:" ",
             1:"N",
             2:"B"
         }
     def __init__(self) -> None:
-        self.board=np.zeros((8,8))
-        self.board[3][3]=1
-        self.board[4][4]=1
-        self.board[3][4]=2
-        self.board[4][3]=2
+        self.plateau=np.zeros((8,8))
+        self.plateau[3][3]=1
+        self.plateau[4][4]=1
+        self.plateau[3][4]=2
+        self.plateau[4][3]=2
 
 
     
@@ -30,8 +32,29 @@ class Plateau:
             str_finale+="\n"+chaine_k
             str_finale+="\n"+ligne_horizontale
         return str_finale
-        
+    
+    def est_sur_plateau(self,x,y)-> bool:
+        if type(x)==type(y)==int and x in range(8) and y in range(8):
+            return True
+        return False    
+    
+    def est_mouvement_valide(self,x,y)->bool:
+        #/!\ATTENTION NE PRENDS PAS EN COMPTE LA CONDITION "le mouvement doit retourner au moins un pion", à faire dans placer_pion
+        liste_position_cardinales=[[0,-1],[1,0],[0,1],[-1,0]]
+        for direction_x,direction_y in liste_position_cardinales:
+            if self.est_sur_plateau(x+direction_x,y+direction_y):
+                if self.plateau[x+direction_x][y+direction_y] != self.plateau[x][y] and self.plateau[x+direction_x][y+direction_y]!=0:
+                    return True
+        return False
+    
+    def placer_pion(self,joueur_id,x,y)->None:
+        pass
             
-            
-x=Plateau()
-print(x)
+x=Plateau() #initialise plateau
+print(x) #sort le contenu de la méthode __str__
+#tests que la fonction est_sur_plateau fonctionne
+print(x.est_sur_plateau(8,4))
+print(x.est_sur_plateau(7,4))
+print(x.est_sur_plateau(7,4.3))
+print(x.est_sur_plateau(7,8))
+#RAS
