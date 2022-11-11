3092f63543331648505f1360ab300c644df113cd
nj1110 <113023482+nj1110@users.noreply.github.com>
2022-11-11 13:50:14 +0100

    Add files via upload

---
+++ MCTS/executionMCTS_v2.py
@@ -0,0 +1,15 @@
+from board_class import Plateau
+from player_class import Joueur
+from MCTS_Noeud_class import MCTS_Noeud
+from MCTSAgent_class import MCTSAgent 
+import random
+import numpy as np
+
+
+plat=Plateau()
+j1=Joueur("Aléatoire", 1) #initialise deux joueurs aléatoires j1 et j2 respectivement noirs et blancs
+j2=Joueur("Aléatoire", 2)
+print("Bienvenue à Othello le jeu trop rigolo")
+print(plat)
+MCTS_A=MCTSAgent(10)
+MCTS_A.Select_un_coup(plat=plat, Joueur_=j1, Autre_Joueur=j2)
