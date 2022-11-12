6b2d5dc273e3bb82a5852a7d851246da0cb64246
nj1110 <113023482+nj1110@users.noreply.github.com>
2022-11-12 02:22:01 +0100

    Add files via upload

---
+++ MCTS/MCTS_Noeud_class.py
@@ -0,0 +1,127 @@
+from board_class import Plateau
+import board_class
+from player_class import Joueur
+import random
+import numpy as np
+
+plat=Plateau()
+print(plat)
+Joueur_=Joueur("Aléatoire",1)
+Autre_Joueur=Joueur("Aléatoire",2)
+
+
+class MCTS_Noeud:
+    
+    def __init__(self, plat, parent=None, coup=[], Joueur_=Joueur_, Autre_Joueur=Autre_Joueur):
+        self.plat=plat
+        self.parent=parent
+        self.coup=coup
+        self.nombre_victoires = {"Joueur_":0,"Autre_Joueur":0}
+        self.nombre_rollouts = {"roll-out":0}
+        self.enfants=[]
+        self.coups_non_visites=plat.liste_coup_valide(Joueur_)
+
+    def ajout_enfant_aleatoire(self, Joueur_, Autre_Joueur):
+        index=random.randint(0, len(self.coups_non_visites)-1)
+        nouveau_coup=self.coups_non_visites.pop(index)
+        nouveau_coup=list(nouveau_coup)
+        print(nouveau_coup)
+        self.plat.placer_pion(Joueur_, nouveau_coup[0], nouveau_coup[1])
+        print("voici le plateau noeud fils aleatoire")
+        print(self.plat)
+        nouveau_noeud=MCTS_Noeud(self.plat, self, nouveau_coup, Autre_Joueur, Joueur_)
+        nouveau_noeud.parent=self
+        self.enfants.append(nouveau_noeud)
+        return nouveau_noeud
+
+    def enregistre_victoire(self, vainqueur):
+        i=self.nombre_victoires["Joueur_"]
+        j=self.nombre_victoires["Autre_Joueur"]
+        if vainqueur=='Joueur_':
+            self.nombre_victoires={"Joueur_":i+1, "Autre_Joueur":j}
+        elif vainqueur=='Autre_Joueur':
+            self.nombre_victoires={"Joueur_":i, "Autre_Joueur":j+1}
+        k=self.nombre_rollouts["roll-out"]
+        self.nombre_rollouts={"roll-out": k+1}
+        return self
+
+    def ajout_enfant_possible(self):
+        self.coups_non_visites=list(self.coups_non_visites)
+        if self.coups_non_visites==[] :
+            return False
+        else:
+            return True
+
+
+    def Est_un_noeud_terminal (self, Joueur_):
+        if plat.liste_coup_valide(Joueur_)==[]:
+            return True
+        else:
+            return False
+
+    def pourcentage_de_victoires (self, Joueur_):
+        return float(self.nombre_victoires["Joueur_"])/float(self.nombre_rollouts["roll-out"])
+
+    def pourcentag_de_victoires (self, Autre_Joueur):
+        return float(self.nombre_victoires["Autre_Joueur"])/float(self.nombre_rollouts["roll-out"])
+
+
+    def Simuler_jeu_aleatoire (self, plat, Joueur_, Autre_Joueur, tour):
+        while plat.fin_de_partie(Joueur_, Autre_Joueur)==False:
+            if tour==2:
+                L=plat.liste_coup_valide(Autre_Joueur)
+                print(L)
+                if (L==[] and plat.liste_coup_valide(Joueur_)==[]):
+                    victoire_=self.plat.victoire()
+                    print(victoire_)
+                    return victoire_
+                elif L==[]:
+                    print("2 ne peut jouer, passe à 1")
+                    tour=1
+                else:
+                    index=random.randint(0, len(L)-1)
+                    nouveau_coup=L.pop(index)
+                    nouveau_coup=list(nouveau_coup)
+                    print(nouveau_coup, "coups pour 2")
+                    self.plat.placer_pion(Autre_Joueur, nouveau_coup[0], nouveau_coup[1])
+                    print("tour passe à 1")
+                    tour=1
+                if plat.fin_de_partie(Joueur_, Autre_Joueur)==True:
+                        victoire_=self.plat.victoire()
+                        print(victoire_)
+                        return victoire_
+            elif tour==1:
+                L=plat.liste_coup_valide(Joueur_)
+                if (L==[] and (plat.liste_coup_valide(Autre_Joueur)==[])):
+                    victoire_=self.plat.victoire()
+                    print(victoire_)
+                    return victoire_
+                elif L==[]:
+                    print("1 ne peut jouer, passe à 2")
+                    tour=2
+                else:
+                    index=random.randint(0, len(L)-1)
+                    nouveau_coup=L.pop(index)
+                    nouveau_coup=list(nouveau_coup)
+                    print(nouveau_coup, "coups pour 1")
+                    self.plat.placer_pion(Joueur_, nouveau_coup[0], nouveau_coup[1])
+                    tour=2
+                    print("tour passe à 2")     
+                if plat.fin_de_partie(Joueur_, Autre_Joueur)==True:
+                        victoire_=self.plat.victoire()
+                        print(victoire_)
+                        return victoire_
+        if plat.fin_de_partie(Joueur_, Autre_Joueur)==True:
+                victoire_=self.plat.victoire()
+                print(victoire_)
+                return victoire_
+                
+    
+
+    
+
+
+
+
+
+
---
+++ MCTS/execution.py
@@ -0,0 +1,38 @@
+from board_class import Plateau
+from player_class import Joueur
+
+plat=Plateau()
+j1=Joueur("MinMax",1,1) #initialise deux joueurs j1 et j2 respectivement noirs et blancs
+j2=Joueur("MinMax",2,1)
+compteur_tour=0
+print("Bienvenue à Othello le jeu trop rigolo")
+while not plat.fin_de_partie(j1,j2):
+    print(f"Tour {compteur_tour}")
+    print(f"tour du joueur {j1.type_joueur} aux pions noir")
+    print(plat)
+    liste_coup_j1=plat.liste_coup_valide(j1)
+    print(f"les coups valables sont : {liste_coup_j1}")
+    if len(liste_coup_j1)>0:
+        x1,y1,prof=j1.get_move(plat,j2)
+        print(x1,y1,prof)
+        plat.placer_pion(j1,x1,y1)
+    else:
+        print("pas de coup possible, sorryyyyyy babe")
+    print(f"tour du joueur {j2.type_joueur} aux pions blancs")
+    print(plat)
+    liste_coup_j2=plat.liste_coup_valide(j2)
+    print(f"les coups valables sont : {liste_coup_j2}")
+    if len(liste_coup_j2)>0:
+        x2,y2,prof=j2.get_move(plat,j1)
+        print(x2,y2,prof)
+        plat.placer_pion(j2,x2,y2)
+    else:
+        print("pas de coup possible, sorryyyyyy babe")
+    compteur_tour+=1
+print("plateau final")
+print(plat)
+scores_finaux=plat.victoire()
+if scores_finaux[0]==scores_finaux[1]:
+    print(f"Avec un score final de {scores_finaux[0]} pour le joueur 1 et de {scores_finaux[1]} pour le joueur 2 la partie finit par une égalité")
+else:
+    print(f"Avec un score final de {scores_finaux[0]} pour le joueur 1 et de {scores_finaux[1]} pour le joueur 2 le vainqueur est le joueur {scores_finaux[2]}")
